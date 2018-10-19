6b0ebfedc353fcd9a1c17e97b1b586ebcd7d28a0
Jaimie <jaimie.walker90@gmail.com>
2018-10-19 00:27:09 -0400

    removed state

--- client/src/Pages/HomeFolder/Home.js
+++ client/src/Pages/HomeFolder/Home.js
@@ -4,17 +4,16 @@ export default class Home extends React.Component {
   componentDidMount () {
     document.title = 'Mildly Adventerous'
   }
-    // document.title = "mildlyadventerous";
-    state = {
-    }
-    render () {
-      return (
 
-        <div>
-          <h1> Welcome to the Adventures of the mildly adventeruous traveler! </h1>
-          <h2 className="lol"> Also known as </h2>
-        </div>
+  // document.title = "mildlyadventerous";
+  render () {
+    return (
 
-      )
-    }
+      <div>
+        <h1> Welcome to the Adventures of the mildly adventeruous traveler! </h1>
+        <h2 className="lol"> Also known as </h2>
+      </div>
+
+    )
+  }
 }
