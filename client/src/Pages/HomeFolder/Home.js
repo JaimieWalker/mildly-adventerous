0505845ad9fea00508e8c2661cebbeb0adbcd8e5
Jaimie <jaimie.walker90@gmail.com>
2018-10-19 01:50:28 -0400

    fixed eslintjson

--- client/src/Pages/HomeFolder/Home.js
+++ client/src/Pages/HomeFolder/Home.js
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
   componentDidMount () {
     document.title = 'Mildly Adventerous'
   }
-  
+
   state = {}
   // document.title = "mildlyadventerous";
   render () {
