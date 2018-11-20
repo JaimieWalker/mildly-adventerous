5a26e4808c796a8ba3355354bddca712a58f4735
Jaimie <jaimie.walker90@gmail.com>
2018-11-20 18:24:03 -0500

    changed some colors

--- client/src/Pages/HomeFolder/NewGame/NewGame.js
+++ client/src/Pages/HomeFolder/NewGame/NewGame.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './NewGame.css'
 export default class NewGame extends React.Component {
   render () {
     return (
-      <section className="name">
-        <p>{"So This is going to be a bit different from all the other games you've played before. In that you will start with just a name, that is the only option you have. Your choices throughout the story will shape, who you are, what you are, what you can even do or even ..... other people"}</p>
+      <section className="explanation">
+        <p>{"So This is going to be a bit different from all the other games you've played before. In that you will start with just a name, that is the only option you have. Your choices throughout the story will shape, who you are, what you are, what you can even do or even ..... other people. If you didn't mean to come here, you can press the back button"}</p>
+        
 
-        <h1><button onClick={() => this.props.history.goBack()}> Back</button></h1>
+        <h2 id="back" className="text-center"><button onClick={() => this.props.history.goBack()}> Back</button></h2>
       </section>
     )
   }
