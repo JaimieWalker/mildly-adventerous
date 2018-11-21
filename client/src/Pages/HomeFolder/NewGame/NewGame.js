080158bc2dd054f5c6d137d1a2fbd6f850d21a27
Jaimie <jaimie.walker90@gmail.com>
2018-11-20 19:02:01 -0500

    added ready button

--- client/src/Pages/HomeFolder/NewGame/NewGame.js
+++ client/src/Pages/HomeFolder/NewGame/NewGame.js
@@ -5,9 +5,11 @@ export default class NewGame extends React.Component {
     return (
       <section className="explanation">
         <p>{"So This is going to be a bit different from all the other games you've played before. In that you will start with just a name, that is the only option you have. Your choices throughout the story will shape, who you are, what you are, what you can even do or even ..... other people. If you didn't mean to come here, you can press the back button"}</p>
-        
 
-        <h2 id="back" className="text-center"><button onClick={() => this.props.history.goBack()}> Back</button></h2>
+        <h2 className="text-center">
+          <button id="ready"> Ready</button>
+          <button id="back" onClick={() => this.props.history.goBack()}> Back</button>
+        </h2>
       </section>
     )
   }
