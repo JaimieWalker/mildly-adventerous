5c1379e9c03eec1c58060c686aed34cfddeb0d8e
Jaimie <jaimie.walker90@gmail.com>
2018-11-23 23:28:38 -0500

    Linked to create page

--- client/src/Pages/HomeFolder/NewGame/NewGame.js
+++ client/src/Pages/HomeFolder/NewGame/NewGame.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './NewGame.css'
+import { Link } from 'react-router-dom'
 export default class NewGame extends React.Component {
   render () {
     return (
@@ -7,7 +8,7 @@ export default class NewGame extends React.Component {
         <p>{"So This is going to be a bit different from all the other games you've played before. In that you will start with just a name, that is the only option you have. Your choices throughout the story will shape, who you are, what you are, what you can even do or even ..... other people. If you didn't mean to come here, you can press the back button"}</p>
 
         <h2 className="text-center">
-          <button id="ready"> Ready</button>
+          <Link to="/create"><button id="ready">Ready</button></Link>
           <button id="back" onClick={() => this.props.history.goBack()}> Back</button>
         </h2>
       </section>
